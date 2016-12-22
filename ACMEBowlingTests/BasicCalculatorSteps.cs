4a80db2bbe5605d983e934b0a2ef77c2a0f7e404
sratliff <sratliff@users.noreply.github.com>
2016-12-22 10:47:57 -0500

    Selenium Unit Tests
    Adding Selenium Driven Unit Tests

---
+++ ACMEBowlingTests/BasicCalculatorSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ACMEBowlingTests
+{
+    [Binding]
+    public class BasicCalculatorSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press subtract")]
+        public void WhenIPressSubtract()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ ACMEBowlingTests/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ACMEBowlingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SeleniumTests tests = new SeleniumTests();
+            tests.Initialize();
+            tests.MyTestMethod();
+            tests.MyTestMethod2();
+            tests.CleanUp();
+        }
+    }
+
+    public class SeleniumTests
+    {
+        internal IWebDriver driver;
+        internal string baseURL;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost:52907/";
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            driver.Navigate().GoToUrl("/Home");
+            Assert.AreEqual(driver.Title, "");
+        }
+
+        [TestMethod]
+        public void MyTestMethod2()
+        {
+            driver.Navigate().GoToUrl("http://localhost:52907/Home/Index");
+
+            driver.FindElement(By.Id("game_Id")).Clear();
+            driver.FindElement(By.Id("game_Id")).SendKeys("0002");
+            driver.FindElement(By.Id("btnEnter")).Click();
+
+            string scoreData = driver.FindElement(By.Id("GameScore")).Text;
+
+            Assert.AreEqual("20", scoreData);
+
+        }
+    }
+}
